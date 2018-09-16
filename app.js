ec70a002a2fc8b7ca80e3b556940bb33cfd17248
sengathit <sengathit.l@gmail.com>
2018-09-16 01:40:37 -0500

    mongo uri

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let express = require('express');
 let mongoose = require('mongoose');
 let bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://sengathit.l@gmail.com:Slavanh77@ds137740.mlab.com:37740/lao-alphabets');
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -12,8 +12,7 @@ const port = process.env.PORT || 300;
 let app = express();
 
 app.get('/api/alphabets',(req,res) => {
-    let alphabets = mongoose.model('Alphabets',{})
-    alphabets.find().then(collection => res.send(collection));
+    res.send('im working');
 });
 
 app.post('/api/save',(req,res) => {
