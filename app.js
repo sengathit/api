0cb9eb296ea4c124e4e43961bf94e2fc9b10a83e
sengathit <sengathit.l@gmail.com>
2018-09-15 23:48:01 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let express = require('express');
+const port = process.env.PORT || 3000;
+
+let app = express();
+
+app.get('/api/alpabets',(req,res) => {
+    res.send('im working');
+});
+
+app.listen(port,() => {
+    console.log('Server started on port ' + port);
+});
