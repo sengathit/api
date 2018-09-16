b05c107b9817289c0fcbbccc8ea7c95958706c8e
sengathit <sengathit.l@gmail.com>
2018-09-16 01:33:32 -0500

    using live mongodb

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 let express = require('express');
-const port = process.env.PORT || 3000;
+let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://sengathit.l@gmail.com:Slavanh77@ds137740.mlab.com:37740/lao-alphabets');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const port = process.env.PORT || 300;
 
 let app = express();
 
 app.get('/api/alphabets',(req,res) => {
-    res.send('im working');
+    let alphabets = mongoose.model('Alphabets',{})
+    alphabets.find().then(collection => res.send(collection));
 });
+
+app.post('/api/save',(req,res) => {
+    let firstName;
+    let lastName;
+})
 
 app.listen(port,() => {
     console.log('Server started on port ' + port);
