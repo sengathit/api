3b6bec52f135e58d51d770aa911e657e0366e05f
sengathit <sengathit.l@gmail.com>
2018-09-18 20:18:51 -0500

    changes

--- app.js
+++ app.js
@@ -5,26 +5,26 @@ let bodyParser = require('body-parser');
 
 let app = express();
 
+const port = process.env.PORT || 3000;
+var db;
+
+mongoose.connect('mongodb://sengathit:Slavanh77@ds137740.mlab.com:37740/lao-alphabets');
+
+let MyModel = mongoose.model('Alphabets',{ name : String});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-const port = process.env.PORT || 3000;
-
 app.get('/',(req,res) => {
+    console.log(db)
     res.send('Home page');
-})
+});
 
 app.get('/api/alphabets',(req,res) => {
-    mongoClient.connect('mongodb://sengathit:Slavanh77@ds137740.mlab.com:37740/lao-alphabets',{ useNewUrlParser: true },(err,db) => {
-        if(err){
-            res.send(err)
-        }else{
-            db.collection('alphabets').find().then(doc => {
-                res.send(docs);
-            });
-        }
-    });
-});
+    MyModel.find().then(docs => {
+        res.send(docs)
+    })
+})
 
 app.listen(port,() => {
     console.log('Server started on port ' + port);
